cf0fc6322002e7d176ce6ed7a95eef9a8be16f1a
Kenneth <github@k.calamay.me>
2011-12-05 10:29:51 +0800

    Added admin namespaced users controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Ticketee::Application.routes.draw do
   resources :projects do
     resources :tickets
   end
+
+  namespace :admin do
+    resources :users
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
